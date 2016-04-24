261365420f377108d9f78ede021aca72f0906ab1
Chih-En Ko <lisa850728g@gmail.com>
2016-04-24 22:07:46 +0800

    work

--- sp/assign3/assign3.c
+++ sp/assign3/assign3.c
@@ -9,49 +9,47 @@ int main(){
     int**  matrix = NULL;
     int i, j, k;
 
-    FILE* file_r = fopen("1.in","r");
-
-    if(file_r == NULL)
-        return -1;
-    else{
-        for(i = 0; i < 5; i++){
-            if(i == 0){
-                fscanf(file_r,"%d",&in_number);     
-                if(in_number >= 0 || in_number <= 15)
-                    use[i] = in_number;
-            }else
+    for(i = 0; i < 5; i++){
+        scanf("%d",&in_number);
+        if(i == 0){
+            if(in_number >= 0 && in_number <= 15)
                 use[i] = in_number;
-        }
+        }else
+            use[i] = in_number;
     }
- 
-    matrix = (int**)malloc(sizeof(int)*use[0];
+    matrix = (int**)malloc(sizeof(int)*use[0]);
     for(i = 0; i < use[0]; i++)
-        matrix[i] = (int*)malloc(sizeof(int)*use[0];
-    }
+        matrix[i] = (int*)malloc(sizeof(int)*use[0]);
 
-    for(j = use[0]; j <= 0; j--){
-        for(k = 0; k <= use[0]; k++){
-            fscanf(file_r,"%d",&in_number);
-            matrix[i][k] = in_number;
+    for(j = use[0]-1; j >= 0; j--){
+        for(k = 0; k < use[0]; k++){
+            scanf("%d",&in_number);
+            matrix[j][k] = in_number;
         }
     }
 
     print_search_results(matrix, use[1], use[2], use[3], use[4]);
 
-    fclose(file_r);
+    return 0;
 }
 
 void print_search_results (int** matrix, int x1, int y1, int x2, int y2){
-    int calculate[10];
-    int i,j,k;
+    int calculate[10] = {0};
+    int p,q,r;
 
-    for(j = x1; j <= x2; j++){
-        for(k = y1; k <= y2; k++)
-            calculate[*(*(matrix+j)+k)] ++;
+    r = 0;
+    for(p = y1; p <= y2; p++){
+        for(q = x1; q <= x2; q++){
+            printf("%d",*(*(matrix+p)+q));
+            calculate[*(*(matrix+p)+q)] ++;
+            r++;
+            if(r % 6 == 0)
+                printf("\n");
+        }
     }
     
-    for(i = 1; i < 10; i++){
-        if(calculate[i] != 0)
-            printf("%d %d", i, calculate[i]);
+    for(r = 1; r < 10; r++){
+        if(calculate[r] != 0)
+            printf("%d %d\n", r, calculate[r]);
     }
 }
